1b32381203b248831702ede1233d75d9b44307c0
JeffredRodriguez <Jeffredra1306@gmail.com>
2026-01-19 12:39:37 -0600

    l

--- src/main/java/com/tiendapesca/APItiendapesca/Security/SecurityConfig.java
+++ src/main/java/com/tiendapesca/APItiendapesca/Security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.tiendapesca.APItiendapesca.Security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -28,34 +29,36 @@ public class SecurityConfig {
         this.jwtFilter = jwtFilter;
     }
 
-    
+
     //Manejo de rutas y roles
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .cors().configurationSource(corsConfigurationSource()).and()
-            .csrf().disable()
-            .authorizeHttpRequests(auth -> auth
-                // PERMITE ACCESO PÚBLICO A RECURSOS ESTÁTICOS
-                .requestMatchers(
-                    "/Imagenes/**",
-                    "/fonts/**",
-                    "/images/**"
-                ).permitAll()
-                .requestMatchers("/auth/**").permitAll()
-                .requestMatchers("/products/**").permitAll()
-                .requestMatchers("/cart/**").authenticated()
-                    .requestMatchers("/orders/**").authenticated()
-                .requestMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated()
-            )
-            .sessionManagement(session -> session
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-            )
-            .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
-        
+                .cors().configurationSource(corsConfigurationSource()).and()
+                .csrf().disable()
+                .authorizeHttpRequests(auth -> auth
+                        // PERMITE ACCESO PÚBLICO A RECURSOS ESTÁTICOS
+                        .requestMatchers(
+                                "/Imagenes/**",
+                                "/fonts/**",
+                                "/images/**"
+                        ).permitAll()
+                        .requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/products/**").permitAll()
+                        .requestMatchers("/cart/**").authenticated()
+                        .requestMatchers(HttpMethod.GET, "/categories/**").permitAll()
+                        .requestMatchers("/orders/**").authenticated()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
+
     //Maneja solicitudes cruzadas para acceder desde diferentes origenes
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
@@ -64,14 +67,13 @@ public class SecurityConfig {
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
         configuration.setExposedHeaders(Arrays.asList("Authorization"));
-        
+
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
     }
 
-    
-    
+
     //Encripta contraseña de Usuario
     @Bean
     public PasswordEncoder passwordEncoder() {
